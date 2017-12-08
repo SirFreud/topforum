8df6e9e259d2d4d73f79a30960eccd81dd3a1a86
Sirfreud <mikevaldez92@gmail.com>
2017-12-08 13:44:37 -0600

    Added that every thread now has a creator to the model. All tests passing

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -6,10 +6,9 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">{{ $thread->title }}</div>
-    
-                <div class="panel-body">w
-                    {{ $thread->body }}
-                </div>
+                    <div class="panel-body">w
+                        {{ $thread->body }}
+                    </div>
             </div>
         </div>
     </div>
@@ -17,16 +16,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             @foreach ($thread->replies as $reply)
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        {{ $reply->owner->name }} said
-                        {{ $reply->created_at->diffForHumans() }}
-                    </div>
-
-                    <div class="panel-body">
-                        {{ $reply->body }}
-                    </div>
-                </div>
+                @include ('threads.reply')
             @endforeach
         </div>
     </div>
