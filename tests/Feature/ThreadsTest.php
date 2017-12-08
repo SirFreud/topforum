08354d836efa3dd99211d20e039b574f4601fb60
Sirfreud <mikevaldez92@gmail.com>
2017-12-07 22:36:19 -0600

    Built second test showing a user can see a single thread. All tests passing

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -10,9 +10,20 @@ class ThreadsTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
-    function a_user_can_browse_threads()
+    function a_user_can_view_all_threads()
     {
+        $thread = factory('App\Thread')->create();
+
         $response = $this->get('/threads');       
-        $response->assertStatus(200);
+        $response->assertSee($thread->title);
+
     }
 }
+    /** @test */
+    function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' . $thread->id);
+        $response->assertSee($thread->title);
+    }
